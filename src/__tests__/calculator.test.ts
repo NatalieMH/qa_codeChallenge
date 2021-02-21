53dad953d0ca2a1ffa7fec356344f5d6c91ef97f
Natalie Halloran <natalie.halloran@yahoo.com>
2021-02-20 22:19:29 -0500

    My Assessment Commit for Calculator.test.ts

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -23,5 +23,43 @@ let dataset = [
   { x: 81, y: 227, method: "multiply" },
   { x: 81, y: 227, method: "divide" },
 ];
-
-describe("Calculator", () => {});
+for(let i =0; i < dataset.length; i++) {
+switch (dataset[i]["method"]) {
+  case "add":
+    console.log(dataset[i]["x"] + dataset[i]["y"]); //Doing my own math
+    console.log(calculator.add(dataset[i]["x"],dataset[i]["y"]));//Passing terms into the right calculator method
+    break;
+  case "subtract":
+    console.log(dataset[i]["x"] - dataset[i]["y"]);
+    console.log(calculator.subtract(dataset[i]["x"],dataset[i]["y"]));
+    break;
+  case "multiply":
+    console.log(dataset[i]["x"]*dataset[i]["y"]);
+    console.log(calculator.multiply(dataset[i]["x"],dataset[i]["y"]));
+    break;
+  case "divide":
+    console.log(dataset[i]["x"]/dataset[i]["y"]);
+    console.log(calculator.divide(dataset[i]["x"],dataset[i]["y"]));
+    break;
+}}
+describe("Calculator", () => { //Testing that my own math equals the calculator functions
+for(let k = 0; k < dataset.length; k++) {
+  if(dataset[k]["method"] == "add") {
+    it("Addition:", () => {
+      expect(dataset[k]["x"] + dataset[k]["y"]).toEqual(calculator.add(dataset[k]["x"],dataset[k]["y"]));
+    }
+    )}
+  if(dataset[k]["method"] == "subtract") {
+    it("Subtraction:", () => {
+      expect(dataset[k]["x"] - dataset[k]["y"]).toEqual(calculator.subtract(dataset[k]["x"],dataset[k]["y"]));
+    }
+    )}
+if(dataset[k]["method"] == "multiply") {
+  it("Multiply:", () => {
+    expect(dataset[k]["x"]*dataset[k]["y"]).toEqual(calculator.multiply(dataset[k]["x"],dataset[k]["y"]));
+  }
+  )}
+if(dataset[k]["method"] == "divide") {
+  it("Divide:", () => {
+    expect(dataset[k]["x"]/dataset[k]["y"]).toEqual(calculator.divide(dataset[k]["x"],dataset[k]["y"]));
+  })}}});
